b7a59928aeaba8eb5bbd3c047a75e0221cce2fc1
João Delfino <joaohsdelfino@gmail.com>
2023-12-04 18:20:22 -0300

    Update regression.py

--- regression.py
+++ regression.py
@@ -12,7 +12,6 @@ def myModel(x):
 x = np.array([0, 50, 100, 150, 200, 255])
 y = np.array([0, 0.6, 1.13, 1.7, 2.27, 3.03])
 
-
 # Linear Regression instantiate
 lin_reg = LinearRegression()
 
@@ -22,12 +21,20 @@ lin_reg.fit(x.reshape(-1,1), y.reshape(-1,1))
 
 slope = lin_reg.coef_[0][0]
 intercept = lin_reg.intercept_[0]
+
 # Getting predict values
 myModel = myModel(x)
+
 # Printing values of coefficients
 print('a0: %.4f' % (lin_reg.intercept_[0]))
 print('a1: %.4f' % (lin_reg.coef_[0][0]))
 
-plt.plot(x, y, '.')
-plt.plot(x, myModel)
+plt.plot(x, y, '.', color='red')
+plt.plot(x, myModel, color='black')
+plt.grid(True)
+plt.title('Linear Regression for Voltage x PWM')
+plt.xlabel('PWM')
+plt.ylabel('Voltage [V]')
+plt.legend(['Real', 'Estimated'])
+plt.savefig('assets/voltage_vs_pwm.png', dpi=400)
 plt.show()
